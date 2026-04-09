551964ac96eaf62eb9b86cc8ee1d8251a6da8dc3
seoyun-lee <20212025@o365.hanbat.ac.kr>
2026-04-09 13:27:56 +0900

    fix: ecosystem.config.js script 경로 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "webfull_9_19_Ant-nest_BE",
-      script: "dist/src/main.js",
+      script: "dist/src/main/main.js",
       instances: "max",
       exec_mode: "cluster",
       watch: false,
