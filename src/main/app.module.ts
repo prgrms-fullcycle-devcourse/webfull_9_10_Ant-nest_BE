8ad8f2932b511028ff79837270281bae8b6a4c64
seoyun-lee <20212025@o365.hanbat.ac.kr>
2026-04-13 14:12:54 +0900

    feat: QuestionModule 추가

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { DiaryModule } from "./diary/diary.module";
+import { QuestionModule } from "./question/question.module";
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
     DiaryModule,
+    QuestionModule
   ],
   controllers: [],
   providers: [],
